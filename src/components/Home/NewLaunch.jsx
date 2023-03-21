d68ac37b983a475e180b4a601f7636fc620eb204
Raiyan109 <kraiyan109@gmail.com>
2023-03-21 21:29:05 +0600

    our services

--- src/components/Home/NewLaunch.jsx
+++ src/components/Home/NewLaunch.jsx
@@ -3,7 +3,27 @@ import layerImg from '../../assets/blob-scene-haikei.svg'
 const NewLaunch = () => {
     return (
         <div>
-            <div style={{ backgroundImage: `url(${layerImg})` }}></div>
+            <div className='w-full aspect-video bg-no-repeat bg-center bg-cover relative' style={{ backgroundImage: `url(${layerImg})` }}></div>
+            <div className='relative lg:left-[800px] lg:bottom-[940px] sm:left-80 sm:bottom-[450px]'>
+                <h1 className='mb-5 lg:text-5xl text-3xl text-[#063806] font-bold'>New Launch</h1>
+            </div>
+            <div className='relative sm:bottom-[500px] lg:bottom-[850px] p-20 lg:p-40'>
+                <h1 className=' lg:text-4xl text-2xl text-[#063806] lg:font-bold font-normal'>Energy Calculator</h1>
+                <div class="sm:w-2/3 sm:py-8  sm:mt-0 text-center sm:text-left">
+                    <div className='max-w-[450px]'>
+                        <p class="leading-relaxed lg:text-sm text-xs mb-4">It is a long established fact that a reader will be distracted by the readable
+                            content of a page when looking at its layout. The point of using Lorem
+                            Ipsum is that it has a more-or-less normal distribution of letters, as
+                            opposed to using 'Content here, content here', making it look like readable
+                            English. Many desktop publishing packages and web page editors now use
+                            Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will
+                            uncover many web sites still in their infancy. Various versions have evolved
+                            over the years, sometimes by accident, sometimes on purpose</p>
+                    </div>
+                    <button className="btn btn-success text-white">Learn More</button>
+                </div>
+
+            </div>
         </div>
     );
 };
--- src/components/Home/OurServices.jsx
+++ src/components/Home/OurServices.jsx
@@ -1,47 +1,68 @@
 import React from 'react';
+import img from '../../assets/pexels-pixabay-159397.jpg'
 
 const OurServices = () => {
     return (
-        <div className='bg-[#8abd8a]'>
+        <div className='bg-[#b6e7b6]'>
             <div className=''>
-                <h1 className="mb-5 pt-20 text-5xl text-[#063806] font-bold text-center">Vedic Energie</h1>
+                <h1 className="mb-5 pt-20 text-5xl text-[#063806] font-bold text-center flex flex-col w-full ">Our Services</h1>
+
+                <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Green solutions can help you in cost-cutting of energy by reducing energy consumption, generating clean energy, taking advantage of
+                    government incentives, and improving building design. By adopting green solutions, individuals and businesses can save money on energy
+                    bills and promote a more sustainable future for al</p>
+
                 <section class="text-gray-600 body-font">
                     <div class="container px-5 py-24 mx-auto flex flex-wrap">
                         <div class="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
-                            <img alt="feature" class="object-cover object-center h-[450px] w-[650px]" src="https://dummyimage.com/460x500" />
+                            <img alt="feature" class="object-cover object-center h-[450px] w-[650px]" src={img} />
                         </div>
                         <div class="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
-                            <div class="flex flex-col mb-10 lg:items-start items-center before:content-['-']">
-                                <div class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
-
+                            <div class="flex items-center border-t border-green-600 p-2 gap-4">
+                                <div class="w-8 h-8 inline-flex items-center justify-center rounded-full bg-indigo-100 text-green-500 mb-5">
                                     &#10003;
                                 </div>
-                                <div class="flex-grow">
-                                    <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Shooting Stars</h2>
-
-
+                                <div class="flex justify-center items-center">
+                                    <h2 class="text-green-900 text-lg title-font font-medium mb-3">Energy Calculation</h2>
                                 </div>
                             </div>
-                            <div class="flex flex-col mb-10 lg:items-start items-center">
-                                <div class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
-
+                            <div class="flex items-center border-t border-green-600 p-2 gap-4">
+                                <div class="w-8 h-8 inline-flex items-center justify-center rounded-full bg-indigo-100 text-green-500 mb-5">
                                     &#10003;
                                 </div>
-                                <div class="flex-grow">
-                                    <h2 class="text-gray-900 text-lg title-font font-medium mb-3">The Catalyzer</h2>
-
-
+                                <div class="flex justify-center items-center">
+                                    <h2 class="text-green-900 text-lg title-font font-medium mb-3">Energy Saving</h2>
                                 </div>
                             </div>
-                            <div class="flex flex-col mb-10 lg:items-start items-center">
-                                <div class="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
-
+                            <div class="flex items-center border-t border-green-600 p-2 gap-4">
+                                <div class="w-8 h-8 inline-flex items-center justify-center rounded-full bg-indigo-100 text-green-500 mb-5">
                                     &#10003;
                                 </div>
-                                <div class="flex-grow">
-                                    <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Neptune</h2>
-
-
+                                <div class="flex justify-center items-center">
+                                    <h2 class="text-green-900 text-lg title-font font-medium mb-3">Energy Demand Management</h2>
+                                </div>
+                            </div>
+                            <div class="flex items-center border-t border-green-600 p-2 gap-4">
+                                <div class="w-8 h-8 inline-flex items-center justify-center rounded-full bg-indigo-100 text-green-500 mb-5">
+                                    &#10003;
+                                </div>
+                                <div class="flex justify-center items-center">
+                                    <h2 class="text-green-900 text-lg title-font font-medium mb-3">Reduce bills and save money</h2>
+                                </div>
+                            </div>
+                            <div class="flex items-center border-t border-green-600 p-2 gap-4">
+                                <div class="w-8 h-8 inline-flex items-center justify-center rounded-full bg-indigo-100 text-green-500 mb-5">
+                                    &#10003;
+                                </div>
+                                <div class="flex justify-center items-center">
+                                    <h2 class="text-green-900 text-lg title-font font-medium mb-3">Energy Supply Solution</h2>
+                                </div>
+                            </div>
+                            <div class="flex items-center border-t border-green-600 p-2 gap-4">
+                                <div class="w-8 h-8 inline-flex items-center justify-center rounded-full bg-indigo-100 text-green-500 mb-5">
+                                    &#10003;
+                                </div>
+                                <div class="flex justify-center items-center">
+                                    <h2 class="text-green-900 text-lg title-font font-medium mb-3">Renewable Energy Solutions</h2>
                                 </div>
                             </div>
                         </div>
